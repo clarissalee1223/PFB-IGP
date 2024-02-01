6d98150eb318ef4ab639783f206412ecf1c5d37c
clarissalee1223 <s10257666@connect.np.edu.sg>
2024-02-01 12:47:19 +0800

    deleted overheads

--- project_group/overheads.py
+++ project_group/overheads.py
@@ -12,31 +12,15 @@ def process_overheads(file_path):
             # Key Category column may be corrupted
             overhead_data.append({'category': row['ï»¿Category'], 'amount': float(row['Overheads'])})
 
-    # Find the highest overhead category
-    highest_overhead_category = find_highest_overhead_category(overhead_data)
 
-    result = {'highest_overhead_category': highest_overhead_category}
+# create a function to determine the highest overhead
+def highest_overhead_value(overhead_data):
+    #for overhead_value in overhead_data:
+    print(overhead_data)
 
-    # Write results to a text file
-    with open("overheads_summary.txt", "w") as summary_file:
-        summary_file.write("Overheads Results:\n")
-        summary_file.write(f"Highest Overhead Category: {result['highest_overhead_category']}\n")
 
-    return result
 
-def find_highest_overhead_category(overhead_data):
-    # Initialize variables to store the highest overhead information
-    highest_overhead_category = ""
-    highest_overhead_value = 0
 
-    # Iterate through each row in the overhead data
-    for row in overhead_data:
-        category = row['category']
-        overhead = row['amount']
 
-        # Check if the current overhead is higher than the current highest overhead
-        if overhead > highest_overhead_value:
-            highest_overhead_value = overhead
-            highest_overhead_category = category
 
-    return highest_overhead_category
+
